891421479dea091040197d9f232f5c71d4d554a9
JosephCoiffman <67018154+JosephCoiffman@users.noreply.github.com>
2021-05-13 15:18:40 -0400

    added stuff

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+from flask import Flask, render_template, json
+import os
+import database.db_connector as db
+
 # Configuration
-
 app = Flask(__name__)
-
+db_connection = db.connect_to_database()
 # Routes 
 
-@app.route('/')
-def root():
-    return "Welcome to the OSU CS 340 - Flask Tutorial!"
+@app.route('/players_select')
+def bsg_people():
+    query = "SELECT * FROM players;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = json.dumps(cursor.fetchall())
+    return results
 
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
+    port = int(os.environ.get('PORT', 9208)) 
     #                                 ^^^^
     #              You can replace this number with any valid port
     
---
+++ database/db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+user = 'coiffmaj'       # don't forget the CS_340 prefix
+passwd = '2152'               # should only be 4 digits if default
+db = 'osu340_coiffmaj'
