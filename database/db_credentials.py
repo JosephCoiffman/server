1e8fe017e23396c904f7591a55fd5d447c2f3eeb
JosephCoiffman <67018154+JosephCoiffman@users.noreply.github.com>
2021-05-13 15:39:20 -0400

    Update db_credentials.py

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -1,4 +1,4 @@
 host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
 user = 'coiffmaj'       # don't forget the CS_340 prefix
-passwd = '2152'               # should only be 4 digits if default
-db = 'osu340_coiffmaj'
+passwd = '2151'               # should only be 4 digits if default
+db = 'cs340_coiffmaj'
